dc497613643715ba4ec8276af36012cea5b70e0f
SIG <13169103974@163.com>
2018-12-04 20:25:02 +0800

    111

---
+++ index.aspx
@@ -0,0 +1,50 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeFile="index.aspx.cs" Inherits="index" %>
+
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+    <title></title>
+    <style type="css/text">
+        
+    </style>
+</head>
+<body>
+    <form id="form1" runat="server">
+    <div align="center">
+        <asp:Label runat="server" Text="图 书 管 理 系 统" Font-Names="微软雅黑" Font-Size="30px"></asp:Label><br />
+        <br />
+        <asp:Image runat="server"  ImageUrl="res/图书馆.png"/><br />
+        <br />
+        <br />
+        &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;账号：<asp:TextBox ID="account" runat="server" BorderStyle="Solid"></asp:TextBox>
+        <asp:RequiredFieldValidator ID="RequiredFieldValidator1" runat="server" 
+            ErrorMessage="RequiredFieldValidator" ControlToValidate="account" 
+            ForeColor="Red">*账号不能为空</asp:RequiredFieldValidator>
+        <br />
+        <br />
+        <br />
+        
+        &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;密码：<asp:TextBox ID="password" runat="server" BorderStyle="Solid"></asp:TextBox>
+        <asp:RequiredFieldValidator ID="RequiredFieldValidator2" runat="server" 
+            ErrorMessage="RequiredFieldValidator" ControlToValidate="password" 
+            ForeColor="Red">*密码不能空</asp:RequiredFieldValidator>&nbsp;&nbsp;
+        <br />
+        <br />
+        <br />
+        <asp:RadioButtonList ID="RadioButtonList1" runat="server" 
+            RepeatDirection="Horizontal">
+            <asp:ListItem>用户</asp:ListItem>
+            <asp:ListItem>管理员</asp:ListItem>
+        </asp:RadioButtonList>
+        <br />
+        <br />
+        <asp:Button ID="Button1" runat="server" Text="登录" BorderColor="White" 
+            Width="61px" onclick="Button1_Click" /><br />
+            <br />
+            <br />
+        <asp:Label ID="Label1" runat="server" Text="" ForeColor="Red"></asp:Label>
+    </div>
+    </form>
+</body>
+</html>
---
+++ index.aspx.cs
@@ -0,0 +1,99 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+using System.Data.SqlClient;
+
+public partial class index : System.Web.UI.Page
+{
+    protected void Page_Load(object sender, EventArgs e)
+    {
+        
+    }
+    protected void RadioButton1_CheckedChanged(object sender, EventArgs e)
+    {
+
+    }
+    protected void Button1_Click(object sender, EventArgs e)
+    {
+        String account1 = account.Text;
+        if (RadioButtonList1.SelectedValue != "用户" && RadioButtonList1.SelectedValue != "管理员")
+        {
+            Response.Write("<script>alert('请选择登录角色！')</script>");
+        }
+        else if (RadioButtonList1.SelectedValue == "用户")
+        {
+            String selectSql = "SELECT * FROM reader Where id=" + account1;
+            Boolean flag = login(selectSql, 1);
+            if (flag == true)
+            {
+                //跳转页面
+            }
+            
+        }
+        else
+        {
+            String selectSql = "SELECT * FROM administrator Where id=" + account1;
+            Boolean flag = login(selectSql, 1);
+            if (flag == true)
+            {
+                //跳转页面
+            }
+        }
+        
+    }
+
+    private Boolean login(String selectSql,int flag)
+    {
+        String account1 = account.Text;
+        String password1 = password.Text;
+        //String selectSql = "SELECT * FROM administrator Where id=" + account1;
+        SqlHelp sqlHelper = new SqlHelp();
+        int count = sqlHelper.SqlServerRecordCount(selectSql);//返回符合的结果数量
+        if (count > 0)//如果信息>0则说明匹配成功
+        {
+            SqlDataReader reader = SqlHelp.GetDataReaderValue(selectSql);
+            while (reader.Read())
+            {
+                //int a = reader.GetInt32(0);
+                try
+                {
+                    String b;
+                    if (flag == 0)  //管理员
+                    {
+                        b = reader.GetString(4);
+                    }
+                    else      //用户
+                    {
+                        b = reader.GetString(5);
+                    }
+                    if (b.Equals(password1))
+                    {
+                        Response.Write("<script>alert('登陆成功')</script>");
+                        return true;
+                    }
+                    else
+                    {
+                        Response.Write("<script>alert('密码错误')</script>");
+                        return false;
+                    }
+                }
+                catch (System.InvalidCastException e)
+                {
+                    Response.Write("<script>alert('系统出错')</script>");
+                    return false;
+                }
+            }
+            reader.Close();
+            SqlHelp.CloseConn();
+        }
+        else
+        {
+            Response.Write("<script>alert('账号不存在')</script>");
+            return false;
+        }
+        return false;
+    }
+}
